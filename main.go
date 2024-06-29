7101b9a4cc2128719a1d6e9cf47ad5257804dc91
Petrus Prinsloo <petrus.prinsloo@gmail.com>
2024-06-29 22:01:08 +0200

    Changed project to use bubble tea instead and now displaying a bit nicer table that can select rows

--- main.go
+++ main.go
@@ -3,11 +3,50 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/charmbracelet/bubbles/table"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
-	"github.com/fatih/color"
-	"github.com/rodaine/table"
+	"os"
 )
+
+var baseStyle = lipgloss.NewStyle().
+	BorderStyle(lipgloss.NormalBorder()).
+	BorderForeground(lipgloss.Color("240"))
+
+type model struct {
+	table table.Model
+}
+
+func (m model) Init() tea.Cmd { return nil }
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "esc":
+			if m.table.Focused() {
+				m.table.Blur()
+			} else {
+				m.table.Focus()
+			}
+		case "q", "ctrl+c":
+			return m, tea.Quit
+		case "enter":
+			return m, tea.Batch(
+				tea.Printf("Let's go to %s!", m.table.SelectedRow()[1]),
+			)
+		}
+	}
+	m.table, cmd = m.table.Update(msg)
+	return m, cmd
+}
+
+func (m model) View() string {
+	return baseStyle.Render(m.table.View()) + "\n"
+}
 
 func main() {
 	cli, err := client.NewClientWithOpts(client.FromEnv)
@@ -20,20 +59,47 @@ func main() {
 		panic(err)
 	}
 
-	for _, ctr := range containers {
-		fmt.Printf("%s\t%s\t%s\t%s\n", ctr.ID, ctr.Image, ctr.Status, ctr.State)
+	columns := []table.Column{
+		{Title: "ID", Width: 64},
+		{Title: "Image", Width: 10},
+		{Title: "Status", Width: 16},
+		{Title: "State", Width: 10},
 	}
 
-	headerFmt := color.New(color.FgGreen, color.Underline).SprintfFunc()
-	columnFmt := color.New(color.FgYellow).SprintfFunc()
+	var rows []table.Row
 
-	tbl := table.New("ID", "Image", "Status", "State")
-	tbl.WithHeaderFormatter(headerFmt).WithFirstColumnFormatter(columnFmt)
-
-	for _, ctr := range containers {
-		fmt.Printf("%s\t%s\t%s\t%s\n", ctr.ID, ctr.Image, ctr.Status, ctr.State)
-		tbl.AddRow(ctr.ID, ctr.Image, ctr.Status, ctr.State)
+	for _, c := range containers {
+		row := table.Row{
+			c.ID,
+			c.Image,
+			c.Status,
+			c.State,
+		}
+		rows = append(rows, row)
 	}
 
-	tbl.Print()
+	t := table.New(
+		table.WithColumns(columns),
+		table.WithRows(rows),
+		table.WithFocused(true),
+		table.WithHeight(7),
+	)
+
+	s := table.DefaultStyles()
+	s.Header = s.Header.
+		BorderStyle(lipgloss.NormalBorder()).
+		BorderForeground(lipgloss.Color("240")).
+		BorderBottom(true).
+		Bold(false)
+	s.Selected = s.Selected.
+		Foreground(lipgloss.Color("229")).
+		Background(lipgloss.Color("57")).
+		Bold(false)
+	t.SetStyles(s)
+
+	m := model{t}
+	if _, err := tea.NewProgram(m).Run(); err != nil {
+		fmt.Println("Error running program:", err)
+		os.Exit(1)
+	}
 }
