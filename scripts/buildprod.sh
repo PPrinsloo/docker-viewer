07eb7bbe33211bb1fe78da05176834faa17c637a
Petrus Prinsloo <petrus.prinsloo@gmail.com>
2024-07-06 22:44:44 +0200

    Build for more platforms and upload them

--- scripts/buildprod.sh
+++ scripts/buildprod.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
 
-CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o container-view
+# Create the build directory if it doesn't exist
+mkdir -p build
+
+# Build for Linux
+CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o build/container-view-linux-amd64
+
+# Build for Mac Intel
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o build/container-view-darwin-amd64
+
+# Build for Mac ARM (Apple Silicon)
+CGO_ENABLED=0 GOOS=darwin GOARCH=arm64 go build -o build/container-view-darwin-arm64
+
+# Build for Windows
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o build/container-view-windows-amd64.exe
+
+# Build for Windows ARM
+CGO_ENABLED=0 GOOS=windows GOARCH=arm64 go build -o build/container-view-windows-arm64.exe
