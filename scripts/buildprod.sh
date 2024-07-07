90dd654864f16ef16051aedc51dd11e26c7cd457
Petrus Prinsloo <petrus.prinsloo@gmail.com>
2024-07-07 12:21:08 +0200

    Added a arm build build linux

--- scripts/buildprod.sh
+++ scripts/buildprod.sh
@@ -6,6 +6,9 @@ mkdir -p build
 # Build for Linux
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -o build/container-view-linux-amd64
 
+# Build for Linux ARM
+CGO_ENABLED=0 GOOS=linux GOARCH=arm64 go build -o build/container-view-linux-arm64
+
 # Build for Mac Intel
 CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o build/container-view-darwin-amd64
 
